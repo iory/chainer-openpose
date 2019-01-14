9f38f3c216c456739388c12bbe706a08c557c338
iory <ab.ioryz@gmail.com>
2019-01-15 06:52:24 +0900

    [transforms] Add resize

---
+++ chainer_openpose/transforms/resize.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+
+def resize(img, ignore_mask, poses, shape):
+    """resize img, mask and annotations"""
+    height, width, _ = img.shape
+
+    resized_img = cv2.resize(img, shape)
+    ignore_mask = cv2.resize(ignore_mask.astype(
+        np.uint8), shape).astype('bool')
+    poses[:, :, :2] = (poses[:, :, :2] * np.array(shape) /
+                       np.array((width, height)))
+    return resized_img, ignore_mask, poses
