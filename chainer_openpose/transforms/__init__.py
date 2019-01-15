6fd3565d736f44311f9aaffe155932d90c63f8e9
iory <ab.ioryz@gmail.com>
2019-01-15 09:01:25 +0900

    [transforms] Modified __init__.py

---
+++ chainer_openpose/transforms/__init__.py
@@ -0,0 +1,5 @@
+from chainer_openpose.transforms.distort_color import distort_color  # NOQA
+from chainer_openpose.transforms.random_crop import random_crop  # NOQA
+from chainer_openpose.transforms.random_resize import random_resize  # NOQA
+from chainer_openpose.transforms.random_rotate import random_rotate  # NOQA
+from chainer_openpose.transforms.resize import resize  # NOQA
