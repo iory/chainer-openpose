1325629686b56a9b7e38f7e841888141c5c04e34
iory <ab.ioryz@gmail.com>
2019-01-31 14:20:50 +0900

    Refactor

--- chainer_openpose/__init__.py
+++ chainer_openpose/__init__.py
@@ -6,8 +6,8 @@ __version__ = pkg_resources.get_distribution(
 
 
 from chainer_openpose import datasets  # NOQA
+from chainer_openpose import extensions  # NOQA
 from chainer_openpose import links  # NOQA
 from chainer_openpose import transforms  # NOQA
 from chainer_openpose import utils  # NOQA
 from chainer_openpose import visualizations  # NOQA
-from chainer_openpose import extensions  # NOQA
